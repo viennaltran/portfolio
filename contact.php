3540091781f664c768cd26d3b8742a06b7361bee
viennaltran <lanvienhtran@gmail.com>
2019-02-18 21:17:49 -0800

    set up phpmailer for contact

--- contact.php
+++ contact.php
@@ -58,5 +58,64 @@ if(isset($_POST['submitted'])) {
         // set our boolean completion value to TRUE
 		$emailSent = true;
 	}
+// from mail_hander.php
+		header('Access-Control-Allow-Origin: *');
+		require_once('email_config.php');
+		require_once('phpmailer/PHPMailer/src/Exception.php');
+		require_once('phpmailer/PHPMailer/src/PHPMailer.php');
+		require_once('phpmailer/PHPMailer/src/SMTP.php');
+		foreach($_POST as $key=>$value){
+			$_POST[$key] = htmlentities( addslashes($value));
+		}
+
+		$mail = new PHPMailer\PHPMailer\PHPMailer;
+		$mail->SMTPDebug = 0;           // Enable verbose debug output. Change to 0 to disable debugging output.
+
+		$mail->isSMTP();                // Set mailer to use SMTP.
+		$mail->Host = 'smtp.gmail.com'; // Specify main and backup SMTP servers.
+		$mail->SMTPAuth = true;         // Enable SMTP authentication
+
+
+		$mail->Username = EMAIL_USER;   // SMTP username
+		$mail->Password = EMAIL_PASS;   // SMTP password
+		$mail->SMTPSecure = 'tls';      // Enable TLS encryption, `ssl` also accepted, but TLS is a newer more-secure encryption
+		$mail->Port = 587;              // TCP port to connect to
+		$options = array(
+			'ssl' => array(
+				'verify_peer' => false,
+				'verify_peer_name' => false,
+				'allow_self_signed' => true
+			)
+		);
+		$mail->smtpConnect($options);
+		$mail->From = 'vtranserver@gmail.com';  // sender's email address (shows in "From" field)
+		$mail->FromName = 'mailer';   // sender's name (shows in "From" field)
+		$mail->addAddress('viennaltran@gmail.com', 'Vienna Tran');  // Add a recipient (name is optional)
+		//$mail->addAddress('ellen@example.com');                        // Add a second recipient
+		$mail->addReplyTo($_POST['email']);                          // Add a reply-to address
+		//$mail->addCC('cc@example.com');
+		//$mail->addBCC('bcc@example.com');
+
+		//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+		//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+		$mail->isHTML(true);                                  // Set email format to HTML
+
+		$mail->Subject = 'mailer message from '.$_POST['email'];
+		$mail->Body    = " 
+			time: ".date('Y-m-d H:is:s')."<br>
+			from: {$_SERVER['REMOTE_ADDR']}<br>
+			name: {$_POST['name']}<br>
+			email: {$_POST['email']}<br>
+			subject: {$_POST['subject']}<br>
+			message: {$_POST['body']}<br>
+		";
+		$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+		if(!$mail->send()) {
+			echo 'Message could not be sent.';
+			echo 'Mailer Error: ' . $mail->ErrorInfo;
+		} else {
+			echo 'Message has been sent';
+		}
 }
 ?>
